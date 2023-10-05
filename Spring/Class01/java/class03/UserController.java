5ec8b46d5d5b8edf1251820e702ea8324b621c4b
minji12344 <140360630+minji12344@users.noreply.github.com>
2023-10-05 18:14:20 +0900

    Add files via upload

---
+++ Spring/Class01/java/class03/UserController.java
@@ -0,0 +1,52 @@
+package class03;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserController
+ */
+@WebServlet("/user")
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<User> users = new ArrayList<>();
+		users.add(new User("철수", 19));
+		users.add(new User("영희", 20));
+		users.add(new User("길동", 21));
+		
+		request.setAttribute("users", users);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/class03/user.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
