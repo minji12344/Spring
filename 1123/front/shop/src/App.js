ff642b62ac1e4d5f3f96b59c423b2df11d137be3
minji12344 <140360630+minji12344@users.noreply.github.com>
2023-11-23 18:17:45 +0900

    Add files via upload

---
+++ 1123/front/shop/src/App.js
@@ -0,0 +1,18 @@
+import { Container } from 'react-bootstrap';
+import './App.css';
+import HeaderPage from './components/HeaderPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FooterPage from './components/FooterPage';
+import NaviPage from './components/NaviPage';
+
+function App() {
+    return (
+        <Container>
+            <HeaderPage/>
+            <NaviPage/>
+            <FooterPage/>
+        </Container>
+    );
+}
+
+export default App;
