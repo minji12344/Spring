d230155a35693cac176cb508243d788e1c288177
minji12344 <140360630+minji12344@users.noreply.github.com>
2023-11-27 18:00:08 +0900

    Add files via upload

---
+++ 1127/back/controller/SearchNaver.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.domain.QueryVO;
+import com.example.domain.ShopSearch;
+
+@Controller
+@RequestMapping("/search")
+public class SearchNaver {
+	@GetMapping("/list")
+	public String list() {
+		return "search.html";
+	}
+	
+	@GetMapping("/list.json")
+	@ResponseBody
+	public String listJSON(QueryVO vo) {
+		return ShopSearch.run(vo);
+	}
+}
