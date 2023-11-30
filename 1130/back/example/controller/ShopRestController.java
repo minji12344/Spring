2e85768994cf9e2e953592d92a86023388907b45
minji12344 <140360630+minji12344@users.noreply.github.com>
2023-11-30 18:19:13 +0900

    Add files via upload

---
+++ 1130/back/example/controller/ShopRestController.java
@@ -0,0 +1,119 @@
+package com.example.controller;
+
+import java.io.File;
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.example.dao.ShopDAO;
+import com.example.domain.QueryVO;
+import com.example.domain.ShopVO;
+import com.example.service.ShopService;
+
+@RestController
+@RequestMapping("/shop")
+public class ShopRestController {
+	@Autowired
+	ShopDAO dao;
+
+	@Autowired
+	ShopService service;
+
+	@GetMapping("/insert/favorites")
+	public void isert(int pid, String uid) {
+		service.insertFavorites(pid, uid);
+	}
+	
+	@GetMapping("/delete/favorites")
+	public void delete(int pid, String uid) {
+		service.deleteFavorites(pid, uid);
+	}
+	
+	@PostMapping("/image")
+	public void image(ShopVO vo, MultipartHttpServletRequest multi) {
+		MultipartFile file = multi.getFile("file");
+		String path = "/upload/shop/";
+		String fileName = System.currentTimeMillis() + ".jpg";
+		try {
+			file.transferTo(new File("c:" + path + fileName));
+			vo.setImage(path + fileName);
+			dao.image(vo);
+		} catch (Exception e) {
+			System.out.println("이미지변경 : " + e.toString());
+		}
+	}
+
+	@PostMapping("/insert")
+	public void insert(@RequestBody ShopVO vo) {
+		service.insert(vo);
+	}
+
+	@GetMapping("/list.json")
+	public HashMap<String, Object> list(QueryVO vo) {
+		return service.list(vo);
+	}
+
+	@GetMapping("/delete")
+	public void delete(int pid) {
+		dao.delete(pid);
+	}
+
+	@GetMapping("/read/{pid}")
+	public HashMap<String, Object> read(@PathVariable int pid) {
+		return dao.read(pid);
+	}
+
+	@GetMapping("/info/{pid}")
+	public HashMap<String, Object> info(@PathVariable int pid, String uid) {
+		return service.read(pid,uid);
+	}
+	
+	@PostMapping("/update")
+	public void update(@RequestBody ShopVO vo) {
+		dao.update(vo);
+	}
+	
+	//에디터사용
+	@PostMapping("/update/content")
+	public void updateContent(@RequestBody ShopVO vo) {
+		dao.updateContent(vo);
+	}
+	
+	@PostMapping("/ckupload/{pid}")
+	   public HashMap<String, Object> ckupload(@PathVariable int pid, MultipartHttpServletRequest multi) {
+	      HashMap<String, Object> map = new HashMap<String, Object>();
+	      
+	      MultipartFile file = multi.getFile("upload");
+	      String path = "/upload/shop/" + pid + "/";
+	      File filePath = new File(path);
+	      if(!filePath.exists()) filePath.mkdir();
+	      
+	      String fileName = System.currentTimeMillis() + ".jpg";
+	      try {
+	         file.transferTo(new File("C:" + path + fileName));
+	         map.put("uploaded", 1);
+	         map.put("url", "/display?file=" + path + fileName);
+	      } catch (Exception e) {
+	         System.out.println("이미지 변경 오류 : " + e.toString());
+	      }
+	      return map;
+	   }
+	
+	//막대그래프
+	@GetMapping("/chart1")
+	public List<HashMap<String, Object>> chart1(){
+		return dao.chart1();
+	}
+
+}
+
+
+
---
+++ 1130/back/example/dao/ReviewDAOImpl.java
@@ -0,0 +1,60 @@
+package com.example.dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.ReviewVO;
+
+@Repository
+public class ReviewDAOImpl implements ReviewDAO{
+	@Autowired
+	SqlSession session;
+	String namespace="com.example.mapper.ReviewMapper";
+	
+	@Override
+	public void insert(ReviewVO vo) {
+		session.insert(namespace + ".insert", vo);
+	}
+
+	@Override
+	public List<HashMap<String, Object>> list(int pid, int page, int size) {
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("pid", pid);
+		map.put("start", (page-1)*size);
+		map.put("size", size);
+		return session.selectList(namespace + ".list", map);
+	}
+
+	@Override
+	public int total(int pid) {
+		return session.selectOne(namespace + ".total", pid);
+	}
+
+	@Override
+	public void delete(int cid) {
+		session.delete(namespace + ".delete", cid);
+	}
+
+	@Override
+	public void updateReviewcnt(int pid, int amount) {
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("pid", pid);
+		map.put("amount", amount);
+		session.update(namespace + ".update_reviewcnt", map);
+	}
+
+	@Override
+	public ReviewVO read(int cid) {
+		return session.selectOne(namespace + ".read", cid);
+	}
+
+	@Override
+	public void update(ReviewVO vo) {
+		session.update(namespace + ".update" , vo);
+	}
+
+}
